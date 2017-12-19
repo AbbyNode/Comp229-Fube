7701ffbd05771ea54051bfe030a1716f1ba32a40
AbbyNode <AbbyNode@users.noreply.github.com>
2017-12-19 13:27:39 -0500

    Added custom tube page, incomplete

---
+++ Comp229-Fube/CustomTube.aspx
@@ -0,0 +1,15 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="CustomTube.aspx.cs" Inherits="Comp229_Fube.CustomTube" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="Head" runat="server">
+    <link href="css/CustomTube.css" rel="stylesheet" />
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="MainContent" runat="server">
+    <div>
+        <h3>Add Ingredient:</h3>
+        <asp:DropDownList ID="IngredientsDropDown" runat="server">
+        </asp:DropDownList>
+        <asp:Button ID="AddIngredientButton" runat="server" Text="Add" OnClick="AddIngredientButton_Click" />
+    </div>
+    <div id="TubeBase">
+    </div>
+</asp:Content>
---
+++ Comp229-Fube/CustomTube.aspx.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Comp229_Fube {
+	public partial class CustomTube : System.Web.UI.Page {
+		public const string SESSION_VAR = "Ingredients";
+
+		private List<string> selectedIngredients;
+
+		protected void Page_Load(object sender, EventArgs e) {
+			if (Session[SESSION_VAR] != null) {
+				selectedIngredients = Session[SESSION_VAR] as List<string>;
+
+				foreach (string s in selectedIngredients) {
+					Response.Write(s);
+				}
+			} else {
+				selectedIngredients = new List<string>();
+			}
+		}
+
+		protected void AddIngredientButton_Click(object sender, EventArgs e) {
+			selectedIngredients.Add("ingredient 1");
+			Session[SESSION_VAR] = selectedIngredients;
+		}
+	}
+}
