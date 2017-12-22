b051b95ea08a3ab5e568a8cf3ce76e97fc43240a
AbbyNode <AbbyNode@users.noreply.github.com>
2017-12-22 01:22:42 -0500

    Added dynamic login/logout button

--- Comp229-Fube/Site.Master.cs
+++ Comp229-Fube/Site.Master.cs
@@ -3,15 +3,29 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
+using System.Web.UI.HtmlControls;
 using System.Web.UI.WebControls;
 
-namespace Comp229_Fube
-{
-    public partial class SiteMaster : MasterPage
-    {
-        protected void Page_Load(object sender, EventArgs e)
-        {
+namespace Comp229_Fube {
+	public partial class SiteMaster : MasterPage {
+		public const string Session_CurrentUser = "CurrentUser";
 
-        }
-    }
+		protected void Page_Load(object sender, EventArgs e) {
+			if (Session[Session_CurrentUser] == null) {
+				HtmlAnchor logIn = new HtmlAnchor();
+				logIn.HRef = "~/LogIn";
+				logIn.InnerText = "Log In";
+				LogInPlaceHolder.Controls.Add(logIn);
+			} else {
+				LinkButton logOut = new LinkButton();
+				logOut.OnClientClick = "LogOut";
+				logOut.Text = "Log Out";
+				LogInPlaceHolder.Controls.Add(logOut);
+			}
+		}
+
+		private void LogOut() {
+			Response.Write("Log Out");
+		}
+	}
 }
--- Comp229-Fube/Site.Master.designer.cs
+++ Comp229-Fube/Site.Master.designer.cs
@@ -22,6 +22,15 @@ namespace Comp229_Fube {
         protected global::System.Web.UI.WebControls.ContentPlaceHolder Head;
         
         /// <summary>
+        /// LogInPlaceHolder control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.PlaceHolder LogInPlaceHolder;
+        
+        /// <summary>
         /// MainContent control.
         /// </summary>
         /// <remarks>
